4a5637710cf067de0fee65cdfe23eae0f9fa17f1
Vu-p <vutran.0110204@gmail.com>
2025-12-25 16:17:37 +0700

    perf(US-103): optimize product query performance

--- Product.java
+++ Product.java
@@ -1,4 +1,10 @@
+import java.util.List;
+
 public class Product {
-    
+    // tối ưu query load product
+    // public List<Product> getProducts() {
+    // return //caigido;
+    // }
+
 }
 // code cg do
