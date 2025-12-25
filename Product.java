32f3001014903e83512268b6c7f79609f127a5d7
Vu-p <vutran.0110204@gmail.com>
2025-12-25 16:19:30 +0700

    feat(US-104): add price filter for products

--- Product.java
+++ Product.java
@@ -1,4 +1,8 @@
+import java.util.List;
+
 public class Product {
-    
+    // public List<Product> filterByPrice(double min, double max) {
+    //     return productRepository.findByPriceRange(min, max);
+    // }
 }
 // code cg do
